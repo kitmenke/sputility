46ef99551b4179b5d3ce9b9879aec3b017f582ea
Kit Menke <cwmenke@gmail.com>
2013-03-16 09:31:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# SPUtility.js
+
+A jquery plugin to modify SharePoint list forms.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/kitmenke/jquery.sputility/master/dist/sputility.min.js
+[max]: https://raw.github.com/kitmenke/jquery.sputility/master/dist/sputility.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/sputility.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ dist/sputility.js
@@ -0,0 +1,34 @@
+/*! SPUtility.js - v0.1.0 - 2013-03-16
+* https://github.com/kitmenke/jquery.sputility
+* Copyright (c) 2013 Kit Menke; Licensed MIT */
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+      // a comment
+    });
+  };
+
+  // Static method.
+  $.awesome = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.awesome.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.awesome.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
